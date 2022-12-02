73092a9c38a7c0627f2a0210969296d1343fee2e
Erick Lara <egmlara81@gmail.com>
2022-12-01 21:30:46 -0300

    feature/new demo project

---
+++ src/main/java/com/erick/lara/sqsintegration/SqsIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.erick.lara.sqsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/erick/lara/sqsintegration/SqsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.lara.sqsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
